54ab33c2cf8ce97b84821f08fcfd18d3822feb9f
irapets777 <irapets777@gmail.com>
2023-01-04 15:33:09 +0200

    slider-ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,41 @@
 let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
+  $('.design__slider').slick({
+    slidesToShow: 4,
     slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
+    dots: false,
+    variableWidth: true,
+    prevArrow: '<img class="arrow left" src="img/arrow-left.svg" alt="">',
+    nextArrow: '<img class="arrow right" src="img/arrow-right.svg" alt="">',
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 4,
+          variableWidth: false,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          variableWidth: false,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          variableWidth: false,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: false,
+        }
+      }
+    ]
   });
 });
 
