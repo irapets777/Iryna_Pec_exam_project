c525ba8f9f74c9087a54d80802baff20620eb97a
Iryna Pec <118050604+irapets777@users.noreply.github.com>
2023-02-21 18:07:55 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,17 @@
-const humb = document.querySelector('#burger-icon');
-const humbBody = document.querySelector('#burger-menu');
-
-
+let humb = document.querySelector('#burger-icon');
+let humbBody = document.querySelector('#burger-menu');
+let humbLink = document.querySelector('#burger-menu');
 humb.addEventListener('click', humbHandler);
 function humbHandler(e) {
    e.preventDefault();
    humbBody.classList.toggle("open");
    humb.classList.toggle('active');
+   humbLink.classList.toggle('active');
 }
-
+$('.burger__menu a').on('click', function(e){
+  $('.burger__menu').toggleClass('open');
+  $('#burger-icon').toggleClass('active');
+})
 
 let slider = $(document).ready(function () {
   $('.design__slider').slick({
